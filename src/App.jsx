93ed4fa11bf9a05a053cbb23e7e6c9c0862f8ead
phamqui <darkscopio2@gmail.com>
2024-10-24 16:31:13 +0700

    #33

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MyComponent from "./components/learn/Mycomponent";
-import {FirstComponent,SecondComponent} from "./components/learn/SecondComponent.jsx";
-import './components/learn/style.css'
 
-
-
-
+import"./components/todo/todo.css"
+import TodoNew from "./components/todo/TodoNew.jsx";
+import TodoData from "./components/todo/TodoData.jsx";
+import reactLogo from "./assets/react.svg"
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <MyComponent />
-        <SecondComponent/>
-        <FirstComponent/>
+    <div className="todo-container">
+        <div className="todo-title">Todo list</div>
+        <TodoNew/>
+        <TodoData/>
+        <div className="todo-image">
+            <img src={reactLogo} className="logo"/>
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-    </>
+        </div>
+    </div>
   )
 }
 
---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,10 @@
+const TodoData = () => {
+    return (
+        <div className="todo-data">
+            <div>Learn reactjs</div>
+            <div>Watching youtube</div>
+        </div>
+    )
+
+}
+export default TodoData ;
