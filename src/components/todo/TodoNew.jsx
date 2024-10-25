35f8b6863e7759e39c479cc5644723b26d4ee365
phamqui <darkscopio2@gmail.com>
2024-10-25 15:02:45 +0700

    #36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,10 +3,21 @@ const TodoNew=(props)=>{
     console.log(">>>",props)
     const {addNewTodo}=props;
     // addNewTodo(name)
+    const handClick=()=>{
+        alert("click me")
+    }
+    const handChage=(name)=>{
+        console.log(">>>hand on change",name)
+    }
+
     return (
         <div className="todo-new">
-            <input type="text"/>
-            <button>Add</button>
+            <input type="text"
+            onChange={(event)=>handChage(event.target.value)}
+            />
+            <button style={{cursor:"pointer"}} onClick={handClick}>Add</button>
+
+
         </div>
     )
 }
