acd3a45cb8deb94173b8f22aa01dd9b5c91c1546
phamqui <darkscopio2@gmail.com>
2024-10-30 14:20:54 +0700

    #51

---
+++ src/components/learn/Mycomponent.jsx
@@ -0,0 +1,12 @@
+import "./style.css"
+const MyComponent = () => {
+    const hoidanit="xin chao cac ban"
+    return (
+        <>
+        <div>{hoidanit} & hello update</div>
+            <div></div>
+        </>
+    );
+}
+export default MyComponent;
+// come here are ne
