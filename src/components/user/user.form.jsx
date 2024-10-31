3ced52810861bc2aaae91bac81f2623120051cd8
phamqui <darkscopio2@gmail.com>
2024-10-31 16:15:01 +0700

    #67

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,27 +1,50 @@
 import {Button, Input} from "antd";
+import {useState} from "react";
 
 const UserForm = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+    const handleSubmit = ()=>{
+        alert(fullName+email+password+phone);
+    }
+
    return (
        <div className="user-form" style={{margin:"20px 0",width:"20%"}}>
            <div style={{display:"flex",gap:"10px",flexDirection:"column"}}>
                <div>
+                   <div>
+                       <p>Full Name: {fullName}</p>
+                       <p>Email: {email}</p>
+                       <p>Password: {password}</p>
+                       <p>Phone: {phone}</p>
+                   </div>
                    <span>FullName</span>
-                   <Input/>
+                   <Input
+                       // value={fullName}
+                       onChange={(event) => setFullName(event.target.value)}
+                   />
+
                </div>
+
                <div>
                    <span>Email</span>
-                   <Input/>
+                   <Input
+                       onChange={(event) => setEmail(event.target.value)}/>
                </div>
                <div>
-                   <span>Password</span>
-                   <Input.Password/>
+               <span>Password</span>
+                   <Input.Password
+                   onChange={(event) => setPassword(event.target.value)}/>
                </div>
                <div>
                    <span>Phone number</span>
-                   <Input/>
+                   <Input
+                   onChange={(event) => setPhone(event.target.value)}/>
                </div>
                <div>
-                   <Button type="primary">Create user</Button>
+                   <Button type="primary" onClick={handleSubmit}>Create user</Button>
                </div>
 
            </div>
